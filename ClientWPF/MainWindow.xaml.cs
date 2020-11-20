5626d8c49b203cfd69ff6fd432e613099567c77a
Daniel Martinho Bisleri <danielbisleri@live.com>
2020-11-20 17:50:03 -0300

    Add Client, Teste API comunication

--- ClientWPF/MainWindow.xaml.cs
+++ ClientWPF/MainWindow.xaml.cs
@@ -1,18 +1,15 @@
-﻿using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClientWPF.Model;
+using MahApps.Metro.Controls;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Threading.Tasks;
+using System;
+using System.Security.Policy;
+using System.Collections.Generic;
+using Domain;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace ClientWPF
 {
@@ -21,9 +18,71 @@ namespace ClientWPF
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        Config conf = new Config() { WebApi = "http://danielbisleri-001-site1.gtempurl.com//api/ClientConnection" };
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private async void btConfig_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await DialogManager.ShowInputAsync(this,"Configuração de Endereço","Atual:"+ conf.WebApi + Environment.NewLine+"Qual o Novo Endereço de API?");
+            conf.WebApi = result;
+        }
+
+        private void btEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            List<ClienteLinks> cliLink = new List<ClienteLinks>();
+            List<ClienteAnexos> cliAnexo = new List<ClienteAnexos>();
+            List<ClienteConexoes> cliConexao = new List<ClienteConexoes>();
+            Clientes cliente = new Clientes();
+
+            cliente.nome = textBoxCliente.Text;
+
+            cliLink.Add(new ClienteLinks()
+            {
+                link = textBoxLink.Text,
+                senha = textBoxPass.Text,
+                usuario = textBoxUser.Text,
+                nome = textBoxNome.Text
+            });
+
+
+            cliente.clientelinks = cliLink;
+
+            cliente.clienteconexao = cliConexao;
+
+            cliente.clienteanexos = cliAnexo;
+
+            var json = JsonConvert.SerializeObject(cliente);
+
+            var client = new RestClient(conf.WebApi);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddJsonBody(json);
+
+
+            IRestResponse response = client.Execute(request);
+
+            var messsageSetting = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "OK"
+            };
+
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                DialogManager.ShowMessageAsync(this, "Enviando", "Cliente Enviado", settings: messsageSetting);
+            }
+            else
+            {
+                DialogManager.ShowMessageAsync(this, "Erro", "Cliente Nào Enviado Enviado", settings: messsageSetting);
+            }
+
+            
+
         }
     }
 }
---
+++ ClientWPF/Model/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientWPF.Model
+{
+    public class Config
+    {
+        public string WebApi { set; get; }
+    }
+}
